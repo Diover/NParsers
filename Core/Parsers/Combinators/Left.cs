98b48661a1013acc5e7509626837114b9e72d246
Diover <istatishere@gmail.com>
2014-05-24 20:30:51 +0400

    [Dev, Tests]. Added "Left" parsers combinator.

---
+++ Core/Parsers/Combinators/Left.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Parsers.Combinators
+{
+    public class Left<TIn, TOut1, TOut2> : IParser<TIn, TOut1>
+    {
+        private readonly IParser<TIn, TOut1> _p;
+
+        public Left(IParser<TIn, TOut1> left, IParser<TIn, TOut2> right)
+        {
+            _p = new Apply<TIn, Tuple<TOut1, TOut2>, TOut1>(new Sequential<TIn, TOut1, TOut2>(left, right),
+                                                            tuple => tuple.Item1);
+        }
+
+        public IList<ParseResult<IList<TIn>, TOut1>> Parse(IList<TIn> input)
+        {
+            return _p.Parse(input);
+        }
+    }
+}
---
+++ Tests/LeftShould.cs
@@ -0,0 +1,34 @@
+﻿using Core.Parsers;
+using Core.Parsers.Combinators;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class LeftShould
+    {
+        [Test]
+        public void ReturnResultOnlyFromLeftParser()
+        {
+            var p = new Left<char, char, char>(new Symbol('a'),
+                                               new Symbol(')'));
+
+            var result = p.Parse("a)".ToCharArray());
+
+            Assert.That(result.Count, Is.EqualTo(1));
+            Assert.That(result[0].Tree, Is.EqualTo('a'));
+            Assert.That(result[0].Remain, Is.Empty);
+        }
+
+        [Test]
+        public void DontReturnResultIfNoMatches()
+        {
+            var p = new Left<char, char, char>(new Symbol('a'),
+                                               new Symbol(')'));
+
+            var result = p.Parse("b)".ToCharArray());
+
+            Assert.That(result, Is.Empty);
+        }
+    }
+}
