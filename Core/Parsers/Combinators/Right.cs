691c2c966743af669324dbc766ed3bd4cfa3f344
Diover <istatishere@gmail.com>
2014-05-24 20:26:44 +0400

    [Dev, Tests]. Added "Right" parsers combinator.

---
+++ Core/Parsers/Combinators/Right.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Parsers.Combinators
+{
+    public class Right<TIn, TOut1, TOut2> : IParser<TIn, TOut2>
+    {
+        private readonly IParser<TIn, TOut2> _p;
+
+        public Right(IParser<TIn, TOut1> left, IParser<TIn, TOut2> right)
+        {
+            _p = new Apply<TIn, Tuple<TOut1, TOut2>, TOut2>(new Sequential<TIn, TOut1, TOut2>(left, right),
+                                                            tuple => tuple.Item2);
+        }
+
+        public IList<ParseResult<IList<TIn>, TOut2>> Parse(IList<TIn> input)
+        {
+            return _p.Parse(input);
+        }
+    }
+}
---
+++ Tests/RightShould.cs
@@ -0,0 +1,34 @@
+﻿using Core.Parsers;
+using Core.Parsers.Combinators;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class RightShould
+    {
+        [Test]
+        public void ReturnResultOnlyFromRightParser()
+        {
+            var p = new Right<char, char, char>(new Symbol('('),
+                                                new Symbol('a'));
+
+            var result = p.Parse("(a".ToCharArray());
+
+            Assert.That(result.Count, Is.EqualTo(1));
+            Assert.That(result[0].Tree, Is.EqualTo('a'));
+            Assert.That(result[0].Remain, Is.Empty);
+        }
+
+        [Test]
+        public void DontReturnResultIfNoMatches()
+        {
+            var p = new Right<char, char, char>(new Symbol('('),
+                                                new Symbol('a'));
+
+            var result = p.Parse("(b".ToCharArray());
+
+            Assert.That(result, Is.Empty);
+        }
+    }
+}
