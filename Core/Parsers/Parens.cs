e886c515a1587ea6c163b262c5a7fcde80b4545e
Diover <istatishere@gmail.com>
2014-05-24 20:39:22 +0400

    [Refactoring]. Applied "Right" and "Left" combinators in "Parens" parser.

--- Core/Parsers/Parens.cs
+++ Core/Parsers/Parens.cs
@@ -8,14 +8,14 @@ namespace Core.Parsers
     {
         public IList<ParseResult<IList<char>, ITree>> Parse(IList<char> input)
         {
-            var s = new Sequential<char, char, ITree, char, ITree>(new Symbol('('),
-                                                                   new Parens(),
-                                                                   new Symbol(')'),
-                                                                   new Parens());
+            var s1 = new Sequential<char, ITree, ITree>(new Left<char, ITree, char>(new Right<char, char, ITree>(new Symbol('('),
+                                                                                                                 new Parens()),
+                                                                                    new Symbol(')')),
+                                                        new Parens());
 
-            var a = new Apply<char, Tuple<char, Tuple<ITree, Tuple<char, ITree>>>, ITree>(s,
-                                                                                          tuple => new Branch(tuple.Item2.Item1,
-                                                                                                              tuple.Item2.Item2.Item2));
+            var a = new Apply<char, Tuple<ITree, ITree>, ITree>(s1,
+                                                                tuple => new Branch(tuple.Item1,
+                                                                                    tuple.Item2));
 
             var p = new Parallel<char, ITree>(a,
                                               new Epsilon<char, ITree>(new Nil()));
